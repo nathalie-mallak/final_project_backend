4083f5b7e634eba604aeb8f113bad1da3f8622cf
nathalie-mallak <nathalie.mallak@gmail.com>
2020-11-29 00:41:49 -0800

    modified

--- app.js
+++ app.js
@@ -15,19 +15,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
 // used for app.post to accept form data
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({
+    extended: true
+}))
 
 // dev is a format of what is dictated in the console 
 app.use(morgan('dev'))
 
 //sets up where to store POST img
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, 'uploads')
     },
 
     // By default, multer removes file extensions so let's add them back
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
 })
@@ -38,55 +40,21 @@ const fileFilter = (req, file, cb) => {
         cb(null, false);
     }
 }
-const upload = multer({ storage: storage, fileFilter: fileFilter })
+const upload = multer({
+    storage: storage,
+})
 
-// const upload = multer({ storage: storage})
-
-// app.use(multer().single(''))
-app.post('/imgData', upload.single('file'), (req, res, next) => {
-    // console.log(req.files.fname.name)
-    console.log('1');
-
-    let upload = multer({storage: storage}).single('file')
-    upload(req, res, function (err) {
-        if(err) {
-            console.log('2');
-            return err
-        }
-        console.log('3');
-
-    })
-    console.log(upload)
-
-    upload(req, res, function (err) {
-        if (err instanceof multer.MulterError) {
-            console.log('4');
-            // A Multer error occurred when uploading.
-        } else if (err) {
-            console.log('5');
-            // An unknown error occurred when uploading
-        }
-        console.log('natalie');
-        // console.log(req.files.file);
-        console.log('mallak');
-
-        const file = req.file
-        console.log(file);
-        console.log(req.files);
-        console.log('nat');
-        
-        console.log(req.files.file.name)
-        file.mv(`${__dirname}/uploads/${file.name}`, (err, result) => {
-
-            if (err) {
-                console.log('6')
-                return res.status(500).send(err)
-            }
-
-        res.json({fileName: file.name, filePath: `uploads/${file.name}`})
-    })
-        console.log(file.name)
-    })
+app.post('/uploadImage', upload.single('image'), (req, res, next) => {
+    try {
+        return res.status(200).json({
+            message: 'Successfull uploaded Image'
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
 })
 
 // use routes
@@ -121,7 +89,7 @@ app.listen(port, () => console.log('server is up'))
 // app.use(multer().single(''))
 
 // app.post('/upload', upload.single('image'), (req, res, next) => {
-    
+
 //     console.log(req.file);
 
 //     let upload = multer({storage: storage}).single('image')
@@ -136,7 +104,7 @@ app.listen(port, () => console.log('server is up'))
 //         } else if (err) {
 //             // An unknown error occurred when uploading
 //         }
-    
+
 //         const file = req.files.image
 //         file.mv(`${__dirname}/uploads/${file.name}`, err => {
 //             if (err) {
@@ -145,7 +113,7 @@ app.listen(port, () => console.log('server is up'))
 //                 return res.status(500).send(err)
 //             }})
 //     })
-    
+
 //     try {
 //         return res.status(201).json({
 //             message: 'File uploded successfully',
